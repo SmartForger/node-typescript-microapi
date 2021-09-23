bd3a09fe622c0f6109fb4af2c69264bfa5140d1a
SmartForger <23446619+SmartForger@users.noreply.github.com>
2021-09-23 12:54:28 -0700

    Expose public folder

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/public', express.static('public'));
+
 app.use('/people', new PeopleController().getRoutes());
 app.use('/health', new HealthController().getRoutes());
 
