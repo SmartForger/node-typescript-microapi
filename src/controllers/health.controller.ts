369caece24775f2a4d9903a33f7eb9ad9d506acc
SmartForger <23446619+SmartForger@users.noreply.github.com>
2021-10-05 15:06:24 -0700

    Add environment to health endpoint

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { Environments } from '../config/environments';
 import { getRedis } from '../common/redis';
 import { RestController } from '../common/RestController';
 
@@ -6,6 +7,7 @@ export class HealthController extends RestController {
   public async checkHealth(req: Request, res: Response): Promise<void> {
     const result = {
       status: 'pass',
+      environment: Environments.nodeEnv,
       info: {
         cache: 'up',
       },
