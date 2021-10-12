4d9b3c0a456b8a0eee914c94275e772293557295
SmartForger <23446619+SmartForger@users.noreply.github.com>
2021-10-12 14:44:09 -0700

    Update test

--- test/integration/root.spec.ts
+++ test/integration/root.spec.ts
@@ -5,6 +5,6 @@ import app from '../../src/app';
 describe('API Root', () => {
   it('returns api running', async () => {
     const res = await request(app).get('/').expect(200);
-    expect(res.text).toBe('API is running ...');
+    expect(res.text).toBe('API is running...');
   });
 });
