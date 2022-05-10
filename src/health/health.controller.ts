e09811f37b5bf7ce5140ff48f188fce38957e97d
SmartForger <23446619+SmartForger@users.noreply.github.com>
2022-05-10 10:27:37 -0700

    cnt-13706: fix tests

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get } from '@nestjs/common';
-import { InjectSentry, SentryService } from '@ntegral/nestjs-sentry';
 import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
 import { CacheService } from '../common/services/cache/cache.service';
 
@@ -8,18 +7,11 @@ export class HealthController {
   constructor(
     private readonly health: HealthCheckService,
     private cache: CacheService,
-    @InjectSentry() private readonly sentry: SentryService,
   ) {}
 
   @Get()
   @HealthCheck()
   public check() {
-    try {
-      return this.health.check([async () => this.cache.getStatus()]);
-    } catch (error) {
-      this.sentry.instance().captureException(error);
-      /* istanbul ignore next */
-      throw error;
-    }
+    return this.health.check([async () => this.cache.getStatus()]);
   }
 }
